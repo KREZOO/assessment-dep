c044a152ccb6e800b19296775156aea77a7c85ee
KREZOO <krezoo.yt@gmail.com>
2024-06-15 15:09:54 +0300

    fix-01

--- next.config.mjs
+++ next.config.mjs
@@ -3,4 +3,13 @@ const nextConfig = {
    output: 'export'
 }
 
+const path = require('path')
+
+module.exports = {
+   webpack: (config) => {
+      config.resolve.alias['@'] = path.resolve(__dirname, 'assessment-dep/ui')
+      return config
+   }
+}
+
 export default nextConfig
