5d8cb357d70739e5d8fb4c6d157f0f8b46fa6832
KREZOO <krezoo.yt@gmail.com>
2024-06-15 15:42:34 +0300

    fix-10

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
 const nextConfig = {
-   webpack: (config) => {
-      config.resolve.alias['@'] = path.resolve(__dirname)
-      return config
-   },
-   output: 'export',
-   images: {
-      unoptimized: true
-   }
+   output: 'export'
 }
 
 export default nextConfig
